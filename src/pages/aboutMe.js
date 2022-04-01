918edf07bf77123cfac1d6f4601e95a8cb8c90dd
Remembrance <classicrockid24@yahoo.com>
2022-04-01 18:41:05 -0500

    added homePage and started working on aboutMe

---
+++ src/pages/aboutMe.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const aboutMe = () => {
+    <main>
+        <title>About Me</title>
+        <h1>About Me</h1>
+        <p></p>
+    </main>
+}
+
+export default aboutMe
